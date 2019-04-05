5cfb586214680680061ba1de82fe9d5263af3e5b
luckyjc8 <lucky.jonathan.lc@gmail.com>
2019-04-05 19:34:30 +0700

    added main

--- src/implementation/Map.cpp
+++ src/implementation/Map.cpp
@@ -62,24 +62,3 @@ void Map::print()
             cout << " ";
     }
 }
-
-main()
-{
-    Map m;
-    Cell *c;
-    Grassland g(0, 0);
-    g.setGrass(false);
-    Coop coop(0, 1);
-    coop.setGrass(false);
-    Barn barn(0, 2);
-    barn.setGrass(false);
-
-    c = &g;
-    m.setObjectAt(c->getX(), c->getY(), c);
-    c = &coop;
-    m.setObjectAt(c->getX(), c->getY(), c);
-    c = &barn;
-    m.setObjectAt(c->getX(), c->getY(), c);
-
-    m.print();
-}
