4a80bc674dd851928be45ecbd5a6f44e85af0078
vchuardi <44575986+vchuardi@users.noreply.github.com>
2019-04-05 19:34:19 +0700

    mysterious

---
+++ src/implementation/MysteriousEgg.cpp
@@ -0,0 +1,6 @@
+#include "../class/Product/farm-product/MysteriousEgg.h"
+using namespace std;
+
+MysteriousEgg::MysteriousEgg(){
+	price = 15;
+}
---
+++ src/implementation/MysteriousMilk.cpp
@@ -0,0 +1,6 @@
+#include "../class/Product/farm-product/MysteriousMilk.h"
+using namespace std;
+
+MysteriousMilk::MysteriousMilk(){
+	price = 25;
+}
